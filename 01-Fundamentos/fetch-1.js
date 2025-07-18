490953dbd91eb48d14ae19d542ad25a5159a0ee2
Lucero Valdez <lvaldezd.isc91@gmail.com>
2025-07-18 17:06:34 -0600

    fetch

---
+++ 01-Fundamentos/fetch-1.js
@@ -0,0 +1,12 @@
+var request = new XMLHttpRequest();
+
+// se coloca True para que sea una petición asincrona
+request.open('GET','https://reqres.in/api/users',true);
+request.send(null);
+request.onreadystatechange = function(state) {
+    if(request.readyState == 4){
+        var resp = request.response;
+        var resp0 = JSON.parse(resp);
+        console.log(resp0);
+    }
+};
---
+++ 01-Fundamentos/fetch-2.js
@@ -0,0 +1,4 @@
+fetch("https://reqres.in/api/users")
+.then(resp => {
+    console.log(resp);
+});
